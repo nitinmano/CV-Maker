026f6a84ba6f8eaf63d4a5d819fcec160be1bbc1
Manasha <55648552+nitinmano@users.noreply.github.com>
2022-09-19 23:27:15 +0530

    2.0

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
-let x=0;
+let x = 0;
 
 const editCv = () => {
-    x=1;
-    // loop res_details
-    let res_details = document.getElementsByClassName('res_details');
-    for (let i = 0; i < res_details.length; i++) {
-        res_details[i].contentEditable = 'true';
-    }
+  x = 1;
+  // loop res_details
+  let res_details = document.getElementsByClassName("res_details");
+  for (let i = 0; i < res_details.length; i++) {
+    res_details[i].contentEditable = "true";
+  }
 
-    
-
-    
-    var id=document.getElementById("img_clk");
-    id.onclick = function() {
-        imgData=prompt("Enter image URL")
-
-        if(imgData!="")
-            id.src=imgData;
-
-
-    };
-}
-
+  var id = document.getElementById("img_clk");
+  id.onclick = function () {
+    let imgData = prompt("Enter image URL");
+    checkIfImageExists(imgData, (exists) => {
+      if (exists) {
+        id.src = imgData;
+      } else {
+        alert("Image does not exists.");
+      }
+    });
+  };
+};
 
 
-// function changeBarWidth() {
-//     ifx==1:
-//     document.getElementById("1").style.width = "100%";
-// }
 
 function changeBarWidth(id) {
-    if(x==1){
-        a=prompt("Enter in %")
-        document.getElementById("1").style.width=a;
-    }
-    
+  if (x == 1) {
+    a = prompt("Enter in %");
+    document.getElementById(id).style.width = a;
+  }
 }
 
-function printPageArea(areaID){
-    var printContent = document.getElementById(areaID);
-    var WinPrint = window.open('', '', 'width=900,height=650');
-    // add bootstrap css
-    WinPrint.document.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Resume</title><script src="https://kit.fontawesome.com/1625eab60d.js" crossorigin="anonymous"></script><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" media="all"><link rel="stylesheet" href="style.css" media="all"></head>');
-    WinPrint.document.write('<link rel="stylesheet" href="style.css" type="text/css" media="all"/>');
-    WinPrint.document.write(printContent.innerHTML);
-    WinPrint.document.close();
-    WinPrint.focus();
-    WinPrint.print();
-    WinPrint.close();
+function printPageArea(areaID) {
+  var printContent = document.getElementById(areaID);
+  var WinPrint = window.open("", "", "width=900,height=650");
+ 
+  WinPrint.document.write(
+    '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Resume</title><script src="https://kit.fontawesome.com/1625eab60d.js" crossorigin="anonymous"></script><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" ><link rel="stylesheet" href="style.css"></head>'
+  );
+  WinPrint.document.write(
+    '<link rel="stylesheet" href="style.css" type="text/css"/>'
+  );
+  WinPrint.document.write(printContent.innerHTML);
+  WinPrint.document.close();
+  WinPrint.focus();
+  WinPrint.print();
+  WinPrint.close();
 }
 
+// CHECK IF IMAGE EXISTS
+function checkIfImageExists(url, callback) {
+  const img = new Image();
+  img.src = url;
+
+  if (img.complete) {
+    callback(true);
+  } else {
+    img.onload = () => {
+      callback(true);
+    };
+
+    img.onerror = () => {
+      callback(false);
+    };
+  }
+}
