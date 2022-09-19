5b95ad80e38262f4643e566ea3651c204e69d638
Manasha <55648552+nitinmano@users.noreply.github.com>
2022-09-19 23:59:58 +0530

    2.o

---
+++ README.md
@@ -0,0 +1,7 @@
+A highly customisable single page CV website <br>
+Live link:  
+[Try it!](https://nitinmano.github.io/resume-editor.github.io/)
+
+* You can edit your cv
+* You can customise your cv
+* you can take the printout
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let x = 0;
 
 const editCv = () => {
+  document.getElementById("editor").style =
+    "background-color: #f5f5f5; color: #000;border: 1px solid #000;border-radius: 5px;transition: 0.5s;";
   x = 1;
   // loop res_details
   let res_details = document.getElementsByClassName("res_details");
@@ -20,9 +22,6 @@ const editCv = () => {
     });
   };
 };
-
-
-
 function changeBarWidth(id) {
   if (x == 1) {
     a = prompt("Enter in %");
@@ -33,7 +32,7 @@ function changeBarWidth(id) {
 function printPageArea(areaID) {
   var printContent = document.getElementById(areaID);
   var WinPrint = window.open("", "", "width=900,height=650");
- 
+
   WinPrint.document.write(
     '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Resume</title><script src="https://kit.fontawesome.com/1625eab60d.js" crossorigin="anonymous"></script><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" ><link rel="stylesheet" href="style.css"></head>'
   );
