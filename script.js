abb2fe5e126dc227b3651f39d57da81aa39ebb51
Manasha <55648552+nitinmano@users.noreply.github.com>
2022-09-19 22:45:28 +0530

    update 2.0

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
+let x=0;
+
 const editCv = () => {
-    document.getElementById('name').contentEditable = 'true';
-    document.getElementById('title').contentEditable = 'true';
-    document.getElementById('sidebarContent').contentEditable = 'true';
-    document.getElementById('contentBar').contentEditable = 'true';
-
-
+    x=1;
     // loop res_details
     let res_details = document.getElementsByClassName('res_details');
     for (let i = 0; i < res_details.length; i++) {
         res_details[i].contentEditable = 'true';
     }
+
+    
+
     
     var id=document.getElementById("img_clk");
-    id.onclick = function() {id.src=prompt();};
-}
-const printPage = () => {
-    var prtContent = document.getElementById("cv-container");
-    var WinPrint = window.open('', '',);
-    WinPrint.document.write(prtContent.innerHTML);
-    WinPrint.document.close();
-    WinPrint.focus();
-    WinPrint.print();
-    WinPrint.close();
+    id.onclick = function() {
+        imgData=prompt("Enter image URL")
+
+        if(imgData!="")
+            id.src=imgData;
+
+
+    };
 }
 
+
+
+// function changeBarWidth() {
+//     ifx==1:
+//     document.getElementById("1").style.width = "100%";
+// }
+
+function changeBarWidth(id) {
+    if(x==1){
+        a=prompt("Enter in %")
+        document.getElementById("1").style.width=;
+    }
+    
+}
 
 function printPageArea(areaID){
     var printContent = document.getElementById(areaID);
     var WinPrint = window.open('', '', 'width=900,height=650');
     // add bootstrap css
-    WinPrint.document.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />');
-    WinPrint.document.write('<link rel="stylesheet" href="style.css" type="text/css" />');
+    WinPrint.document.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Resume</title><script src="https://kit.fontawesome.com/1625eab60d.js" crossorigin="anonymous"></script><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" media="all"><link rel="stylesheet" href="style.css" media="all"></head>');
+    WinPrint.document.write('<link rel="stylesheet" href="style.css" type="text/css" media="all"/>');
     WinPrint.document.write(printContent.innerHTML);
     WinPrint.document.close();
     WinPrint.focus();
